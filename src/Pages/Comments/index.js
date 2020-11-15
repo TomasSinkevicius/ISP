2aa364a89f0181e8178e30bc6c3ac75bc6967167
tomel1s <tomsin418@gmail.com>
2020-11-15 20:03:28 +0200

    Added comment section

---
+++ src/Pages/Comments/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import comments from "../../assets/css/comments.scss";
+function index() {
+  return (
+    <div className="comment-section">
+      <div className="comment">
+        <div className="comment-author">Alex123</div>
+        <div className="comment-text">I think thats a great movie!</div>
+        <div class="star-rating">
+          <input type="radio" id="5-stars" name="rating" value="5" />
+          <label for="5-stars" class="star">
+            &#9733;
+          </label>
+          <input type="radio" id="4-stars" name="rating" value="4" />
+          <label for="4-stars" class="star">
+            &#9733;
+          </label>
+          <input type="radio" id="3-stars" name="rating" value="3" />
+          <label for="3-stars" class="star">
+            &#9733;
+          </label>
+          <input type="radio" id="2-stars" name="rating" value="2" />
+          <label for="2-stars" class="star">
+            &#9733;
+          </label>
+          <input type="radio" id="1-star" name="rating" value="1" />
+          <label for="1-star" class="star">
+            &#9733;
+          </label>
+        </div>
+        <button className="comment-reply" type="button">
+          Reply
+        </button>
+      </div>
+
+      <div className="write-comment">
+        <textarea
+          placeholder="Write your comment here!"
+          class="write-comment-text"
+        ></textarea>
+        <button class="write-comment-button" type="button">
+          Comment
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default index;
+
+{
+  /* <div className="comment">
+<a className="comment-author">User123</a>
+<p className="comment-text">Very good movie!!</p>
+</div>
+
+<div>
+<textarea
+  placeholder="Write your comment here!"
+  class="pb-cmnt-textarea"
+></textarea>
+<button class="btn btn-primary pull-right" type="button">
+  Comment
+</button>
+</div> */
+}
--- src/Pages/Details/index.js
+++ src/Pages/Details/index.js
@@ -7,72 +7,90 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-
 import { useHistory } from "react-router-dom";
+import Comments from "../Comments/index.js";
 
-const cards = [1,2,3,4,5,6];
+const cards = [1, 2, 3, 4, 5, 6];
 
 const DetailsScreen = (props) => {
+  const history = useHistory();
 
-    const history = useHistory();
-
-    return (
-      <div className='background-blur-wrapper'>
-          <div className='movie-details-container'>
-            <div className='movie-details-content-container'>
-                <h1 className='movie-details-title'>
-                    Movie title
-                </h1>
-                <p className='movie-details-description'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <div className='movie-details-button-section'>
-                    <Button size="large" color="primary">
-                        Play
-                    </Button>
-                    <Button size="large" color="primary">
-                        Add to favorites
-                    </Button>
-                </div>
-            </div>
-            <div className='movie-details-related-section'>
-                <h2 className='movie-details-title-secondary'>
-                    Related movies
-                </h2>
-                <Grid container direction="row" justify="flex-start" alignItems="center">
-                    {cards.map((card, index) => (
-                    <Grid item key={card}>
-                        <Card className='movie-details-related-section-card'>
-                            <CardMedia
-                                className='movie-details-related-section-cardmedia'
-                                image="https://source.unsplash.com/random"
-                                title="Image title"
-                            />
-                            <CardContent className='movie-details-related-section-cardcontent'>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                Heading
-                                </Typography>
-                                <Typography>
-                                This is a media card. You can use this section to describe
-                                the content.
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button onClick={() => history.push(`/movie/${index}`)} size="small" color="primary">
-                                View
-                                </Button>
-                                <Button size="small" color="primary">
-                                Edit
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    ))}
-                </Grid>
-            </div>
+  return (
+    <div className="background-blur-wrapper">
+      <div className="movie-details-container">
+        <div className="movie-details-content-container">
+          <h1 className="movie-details-title">Movie title</h1>
+          <p className="movie-details-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="movie-details-button-section">
+            <Button size="large" color="primary">
+              Play
+            </Button>
+            <Button size="large" color="primary">
+              Add to favorites
+            </Button>
           </div>
+        </div>
+        <Comments />
+        <div className="movie-details-related-section">
+          <h2 className="movie-details-title-secondary">Related movies</h2>
+          <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="center"
+          >
+            {cards.map((card, index) => (
+              <Grid item key={card}>
+                <Card className="movie-details-related-section-card">
+                  <CardMedia
+                    className="movie-details-related-section-cardmedia"
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className="movie-details-related-section-cardcontent">
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      onClick={() => history.push(`/movie/${index}`)}
+                      size="small"
+                      color="primary"
+                    >
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default DetailsScreen;
