2f652168fbed5d1a925f71d3c994a22fa7244aa7
tomel1s <tomsin418@gmail.com>
2020-11-15 13:19:15 +0200

    Created main layout

---
+++ src/Components/Content/content.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../App.scss";
+import "./content.scss";
+
+function content() {
+  return <main className="page__body"></main>;
+}
+export default content;
---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import NotificationBell from "../../assets/images/breadcrumb-separator.svg";
+import UserProfileWidget from "../../assets/images/placeholder-profile.jpg";
+import "./header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__top-nav">
+        <a class="header__top-nav-item">Apie mus</a>
+        <a class="header__top-nav-item">Filmai</a>
+        <a class="header__top-nav-item">Top filmai</a>
+        <a class="header__top-nav-item">Forumas</a>
+        <figure className="header__top-nav-item user-profile-widget">
+          <img src={UserProfileWidget} alt="user profile" />
+        </figure>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
